cd71d0b92f5bb03f71db5b66c06babb9df030d3c
森崎善喬 <y.morisaki.c.a@gmail.com>
2026-08-01 12:48:00 +0900

    v0.7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='morisaki-v062';
+const CACHE='morisaki-v07';
 const ASSETS=['./?v=062','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
