17b5e55b4a9ef6ce405531bdc8f7825a20d117a5
森崎善喬 <y.morisaki.c.a@gmail.com>
2026-08-03 14:01:42 +0900

    v0.9.1 OCR修正

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='morisaki-v09';
+const CACHE='morisaki-v091';
 const ASSETS=['./?v=062','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
